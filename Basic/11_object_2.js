27d38088b26da73a29e966f7fdd4d174fdd22c2e
mishuk09 <mishukinfo09@gmail.com>
2025-03-20 13:57:36 +0600

    learn object

--- Basic/11_object_2.js
+++ Basic/11_object_2.js
@@ -17,4 +17,23 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullName?.userfullname.firstname);
+// console.log(regularUser.fullName?.userfullname.firstname);
+
+const obj1 = {
+    1: "a", 2: "b"
+
+}
+const obj2 = {
+    1: "ac", 2: "bc"
+
+}
+
+const obj3 = Object.assign({}, obj1, obj2)
+
+console.log(obj3);
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('islogedIn'));
